7ee2680fdcd14baa8b5816f6123174b0743a151c
Guilherme Machado da Silva <150199325+GuiXado@users.noreply.github.com>
2025-09-10 01:35:05 -0300

    Add files via upload

---
+++ src/controller/Triatlo.java
@@ -0,0 +1,80 @@
+package controller;
+
+import java.util.concurrent.Semaphore;
+
+public class Triatlo extends Thread {
+	
+	private int id;
+	private Semaphore semaforo;
+	private static int pontChegadaCorrida = 260;
+	private int pontCorrida;
+	private static int pontChegadaCiclismo = 260;
+	private int pontCiclismo;
+	private int pontTotal;
+    public static int[] resultados = new int[25];
+	
+	public Triatlo(int id, Semaphore semaforo) {
+		this.id = id;
+		this.semaforo = semaforo;
+	}
+	
+	public void run() {
+		corrida();
+		
+		try {
+			semaforo.acquire();
+			tiro();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			semaforo.release();
+		}
+		ciclismo();
+		
+		total();
+	}
+
+	private void corrida() {
+		int distCorrida = 0;
+		int distTotal = 3000;
+		while (distCorrida <= distTotal) {
+			distCorrida += (int)((Math.random() *6 ) + 20);
+			try {
+				sleep(30);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} 
+		}
+		pontChegadaCorrida -= 10;
+		pontCorrida = pontChegadaCorrida;
+	}
+
+	private int pontTiro = 0;
+	private void tiro() {
+		for (int i = 1; i < 4; i++) {
+			pontTiro += (int)(Math.random() * 11);
+		}
+	}
+
+	private void ciclismo() {
+		int distCiclismo = 0;
+		int distTotalC = 5000;
+		while (distCiclismo <= distTotalC) {
+		distCiclismo += (int)((Math.random() *11 ) + 30);
+			try {
+				sleep(40);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		pontChegadaCiclismo -= 10;
+		pontCiclismo = pontChegadaCiclismo;
+	}
+	
+	private void total() {
+		pontTotal = pontCorrida + pontTiro + pontCiclismo;
+		resultados[id-1] = pontTotal;
+		//System.out.println("#"+id+" pontua "+pontTotal);
+	}
+	
+}
---
+++ src/view/Atleta.java
@@ -0,0 +1,42 @@
+package view;
+
+import java.util.concurrent.Semaphore;
+import controller.Triatlo;
+
+public class Atleta {
+
+	public static void main(String[] args) {
+		Semaphore semaforo = new Semaphore(5);
+		Triatlo[] atetlas = new Triatlo[25];
+
+		for (int id = 1; id <= 25; id++) {
+			atetlas[id - 1] = new Triatlo(id, semaforo);
+			atetlas[id - 1].start();
+		}
+
+		for (int i = 0; i < 25; i++) {
+			try {
+				atetlas[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		for (int i = 0; i < 25; i++) {
+			for (int j = i + 1; j < 25; j++) {
+				if (Triatlo.resultados[i] < Triatlo.resultados[j]) {
+					int temp = Triatlo.resultados[i];
+					Triatlo.resultados[i] = Triatlo.resultados[j];
+					Triatlo.resultados[j] = temp;
+				}
+			}
+		}
+
+		System.out.println("Resultados finais:");
+		for (int i = 0; i < 25; i++) {
+			System.out.println((i + 1) + "º lugar: " + Triatlo.resultados[i]);
+		}
+
+	}
+
+}
